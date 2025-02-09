4e2c385072c6292aab4c9afc226c757a85047f0a
LouiseYLi <louiseli.van@gmail.com>
2025-02-09 10:15:54 -0800

    modified dictionary to account for null word

--- COMP4537/labs/4/js/dictionary.js
+++ COMP4537/labs/4/js/dictionary.js
@@ -3,15 +3,16 @@ class Dictionary {
         this.dictionary = new Map();
     }
     add_entry(word, definition) {
-        console.log(word.toLowerCase());
-        if (!this.dictionary.has(word.toLowerCase())) {
+        if (word && !this.dictionary.has(word.toLowerCase())) {
             this.dictionary.set(word.toLowerCase(), definition);
             return word;
         }
         else return null;
     }
     get_definition(word) {
-        if (!this.dictionary.has(word.toLowerCase())) return null;
+        if (!word || !this.dictionary.has(word.toLowerCase())) {
+            return null;
+        }
         return this.dictionary.get(word.toLowerCase());
     }
 }
